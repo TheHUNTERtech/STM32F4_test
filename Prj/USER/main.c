7b671a23bf6b9b582d737579b0f70f84ba38208e
Trìnhđộ <huutrinh999@gmail.com>
2018-03-21 19:36:51 +0700

    post comment
    comment guide

--- Prj/USER/main.c
+++ Prj/USER/main.c
@@ -7,19 +7,19 @@ int a=0;
 int main(void)
 { 
 	delay_init(168);
-	LED_Init();
+	LED_Init();	//config Output pin
 	
 	while(1)
 	{
-		GPIO_ResetBits(GPIOA,GPIO_Pin_6);
+		GPIO_ResetBits(GPIOA,GPIO_Pin_6);	//set PA06 low
 		a++;
 		if(a >= 3 ){
-			GPIO_ResetBits(GPIOA,GPIO_Pin_7);
+			GPIO_ResetBits(GPIOA,GPIO_Pin_7);	//set PA07 low
 			a=0;
 		}
 		delay_ms(500);
-		GPIO_SetBits(GPIOA, GPIO_Pin_6);
-		GPIO_SetBits(GPIOA, GPIO_Pin_7);
+		GPIO_SetBits(GPIOA, GPIO_Pin_6);	//set PA06 high
+		GPIO_SetBits(GPIOA, GPIO_Pin_7);	//set PA07 high
 		delay_ms(500);
 	}
 }
